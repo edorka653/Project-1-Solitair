ebf06dc0175a8ee83838b9fef85fe9ca2698a650
edorka653 <56091734+edorka653@users.noreply.github.com>
2022-01-30 12:31:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ import pygame
 import os
 import sys
 
+all_sprites = pygame.sprite.Group()
 
-class Board:
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Board(pygame.sprite.Sprite):
+    def __init__(self, x, y, *group):
+        super().__init__(*group)
+        self.image = load_image("Board.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Setka:
     def __init__(self, cell_x, cell_y):
         self.cell_x = cell_x
         self.cell_y = cell_y
@@ -15,125 +35,146 @@ class Board:
     def draw(self, screen):
         for i in range(self.cell_y):
             for j in range(self.cell_x):
-                if self.board[i][j] == 0:
+                if self.board[j][i] == 0:
                     pygame.draw.rect(screen, pygame.Color('white'),
-                                 (self.left + j * self.cell_size, self.top + i * self.cell_size, self.cell_size,
-                                  self.cell_size), 1)
+                                     (self.left + j * self.cell_size, self.top + i * self.cell_size,
+                                      self.cell_size,
+                                      self.cell_size), 1)
+
                 else:
-                    pygame.draw.rect(screen, pygame.Color('white'),
-                                 (self.left + j * self.cell_size, self.top + i * self.cell_size, self.cell_size,
-                                  self.cell_size))
+                    pygame.draw.rect(screen, pygame.Color('green'),
+                                     (self.left + j * self.cell_size, self.top + i * self.cell_size,
+                                      self.cell_size,
+                                      self.cell_size), 1)
 
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
 
-    def get_click(self, mouse_pos):
+    def get_click_left(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
+        self.on_click_left(cell)
+        return cell
+
+    def get_click_right(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click_right(cell)
+        return cell
 
     def get_cell(self, mouse_pos):
         x, y = mouse_pos
         cell_x = (x - self.left) // self.cell_size
         cell_y = (y - self.top) // self.cell_size
-        if 0 <= cell_x <= self.cell_x and 0 <= cell_y <= self.cell_y:
+        if 0 <= cell_x <= self.cell_x and 0 <= cell_y <= self.cell_y and \
+                not ((cell_y < 2 or cell_y > 4) and (cell_x < 2 or cell_x > 4)):
             return cell_x, cell_y
         else:
             return None
 
-    def on_click(self, cell):
+    def on_click_left(self, cell):
         if cell:
-            for i in range(self.cell_x):
-                self.board[cell[1]][i] = (self.board[cell[1]][i] + 1) % 2
-            for i in range(self.cell_y):
-                self.board[i][cell[0]] = (self.board[i][cell[0]] + 1) % 2
-            self.board[cell[1]][cell[0]] = (self.board[cell[1]][cell[0]] + 1) % 2
-            print(cell)
+            for i in range(7):
+                for j in range(7):
+                    self.board[i][j] = 0
+            self.board[cell[0]][cell[1]] = (self.board[cell[0]][cell[1]] + 1) % 2
+            return cell
+
+    def on_click_right(self, cell):
+        main_pos = 0
+        for i in range(7):
+            for j in range(7):
+                if self.board[i][j]:
+                    main_pos = i, j
+        if cell and main_pos:
+            if cell[0] == (main_pos[0] + 2) and cell[1] == main_pos[1]:
+                if ball[cell[1]][cell[0]] and ball[cell[1]][cell[0] - 1] and \
+                        not (ball[cell[1]][cell[0] - 2]):
+                    ball[cell[0]][cell[1]].rect.move(-170, 0)
+                    ball[cell[0]][cell[1]], ball[cell[0] - 2][cell[1]] = \
+                        ball[cell[0] - 2][cell[1]], ball[cell[0]][cell[1]]
+                    ball[cell[0]][cell[1]] = 0
+            elif cell[0] == (main_pos[0] - 2) and cell[1] == main_pos[1]:
+                return
+            elif cell[1] == (main_pos[1] + 2) and cell[0] == main_pos[0]:
+                return
+            elif cell[1] == (main_pos[1] - 2) and cell[0] == main_pos[0]:
+                return
+            else:
+                return
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, y, *group):
+        super().__init__(*group)
+        self.image = load_image("Ball.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, event_pos, type):
+        if type == 3:
+            pos_move = Sr.get_click_right(event_pos)
+            if pos_move is None:
+                return
+            if not ((pos_move[0] < 2 or pos_move[0] > 4) and
+                    (pos_move[1] < 2 or pos_move[1] > 4)):
+                return
+            Sr.board[pos_move[0]][pos_move[1]] = 1
+            my_pos = (self.rect.left, self.rect.top)
+            if pos_move[0] != my_pos[0] or pos_move[1] == my_pos[1]:
+                print('Можно сделать ход')
+            elif pos_move[0] == my_pos[0] or pos_move[1] != my_pos[1]:
+                print('Можно сделать ход')
+        if type == 1:
+            pos_move = Sr.get_click_left(event_pos)
 
 
 if __name__ == '__main__':
-    def load_image(name, colorkey=None):
-        fullname = os.path.join('data', name)
-        if not os.path.isfile(fullname):
-            print(f"Файл с изображением '{fullname}' не найден")
-            sys.exit()
-        image = pygame.image.load(fullname)
-        return image
-
-
-    def draw_board():
-        boar_image = load_image('Board.png')
-        screen.blit(boar_image, (1, 1))
-
-
-    def draw_ball():
-        ball_sprite_grup_draw = pygame.sprite.Group()
-        ball_sprite = pygame.sprite.Sprite()
-        ball_sprite.image = load_image("Ball.png")
-        ball_sprite.rect = ball_sprite.image.get_rect()
-
-        for x in range(3):
-            for y in range(2):
-                ball_sprite.rect.x = 388 + x * 84
-                ball_sprite.rect.y = 53 + y * 84
-                ball_sprite_grup_draw.add(ball_sprite)
-                ball_sprite_grup.add(ball_sprite)
-                ball_sprite_grup_draw.draw(screen)
-
-        for x in range(3):
-            for y in range(3):
-                ball_sprite.rect.x = 217 + x * 85
-                ball_sprite.rect.y = 223 + y * 84
-                ball_sprite_grup_draw.add(ball_sprite)
-                ball_sprite_grup.add(ball_sprite)
-                ball_sprite_grup_draw.draw(screen)
-
-        for x in range(3):
-            for y in range(3):
-                ball_sprite.rect.x = 472 + x * 85
-                ball_sprite.rect.y = 222 + y * 84
-                if not (ball_sprite.rect.x == 472 and ball_sprite.rect.y == 306):
-                    ball_sprite_grup_draw.add(ball_sprite)
-                    ball_sprite_grup.add(ball_sprite)
-                    ball_sprite_grup_draw.draw(screen)
-
-        for x in range(1):
-            for y in range(3):
-                ball_sprite.rect.x = 725 + x * 85
-                ball_sprite.rect.y = 222 + y * 83
-                ball_sprite_grup_draw.add(ball_sprite)
-                ball_sprite_grup.add(ball_sprite)
-                ball_sprite_grup_draw.draw(screen)
-
-        for x in range(3):
-            for y in range(2):
-                ball_sprite.rect.x = 390 + x * 84
-                ball_sprite.rect.y = 478 + y * 84
-                ball_sprite_grup_draw.add(ball_sprite)
-                ball_sprite_grup.add(ball_sprite)
-                ball_sprite_grup_draw.draw(screen)
 
     pygame.init()
     size = width, height = 1000, 700
     screen = pygame.display.set_mode(size)
     running = True
-    cloak = pygame.time.Clock()
+    clock = pygame.time.Clock()
     FPS = 30
-    ball_sprite_grup = pygame.sprite.Group()
-    Br = Board(7, 7)
+    Board(1, 1, all_sprites)
+    ball = [[0] * 7 for i in range(7)]
+    for x in range(2, 5):
+        for y in range(2):
+            ball[y][x] = Ball(388 + (x - 2) * 84, 53 + y * 84, all_sprites)
 
+    for x in range(3):
+        for y in range(2, 5):
+            ball[y][x] = Ball(217 + x * 85, 223 + (y - 2) * 84, all_sprites)
+    for x in range(3, 6):
+        for y in range(2, 5):
+            X = 472 + (x - 3) * 85
+            Y = 222 + (y - 2) * 84
+            if not (X == 472 and Y == 306):
+                ball[y][x] = Ball(472 + (x - 3) * 85, 222 + (y - 2) * 84, all_sprites)
+    for x in range(6, 7):
+        for y in range(2, 5):
+            ball[y][x] = Ball(725 + (x - 6) * 85, 222 + (y - 2) * 83, all_sprites)
+
+    for x in range(2, 5):
+        for y in range(5, 7):
+            ball[y][x] = Ball(390 + (x - 2) * 84, 478 + (y - 5) * 84, all_sprites)
+    all_sprites.draw(screen)
+
+    Sr = Setka(7, 7)
     while running:
-        draw_board()
-        draw_ball()
-        Br.draw(screen)
         pygame.display.flip()
-        cloak.tick(FPS)
+        clock.tick(FPS)
+        all_sprites.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    Br.get_click(event.pos)
-
+                    Sr.get_click_left(event.pos)
+                if event.button == 3:
+                    all_sprites.update(event.pos, 3)
+        all_sprites.draw(screen)
+        Sr.draw(screen)
     pygame.quit()
